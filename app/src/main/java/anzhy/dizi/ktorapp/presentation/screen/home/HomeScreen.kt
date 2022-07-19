6af960be39e6f9c74875c456de969eddcaafb70b
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-19 18:17:35 -0400

    add rating calculation

--- app/src/main/java/anzhy/dizi/ktorapp/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/anzhy/dizi/ktorapp/presentation/screen/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package anzhy.dizi.ktorapp.presentation.screen.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import anzhy.dizi.ktorapp.presentation.components.RatingWidget
+import anzhy.dizi.ktorapp.ui.theme.LARGE_PADDING
 import com.google.accompanist.pager.ExperimentalPagerApi
 
 @ExperimentalPagerApi
@@ -19,5 +23,8 @@ fun HomeScreen(
         topBar = {
             HomeTopBar(onSearchClicked = {})
         }
-    ) {}
+    ) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING), rating = 3.3)
+    }
 }
