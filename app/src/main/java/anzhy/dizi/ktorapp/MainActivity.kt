1275b4daa8e881c7678051defda0b1d4553f86c4
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-21 17:26:30 -0400

    implement initialize fun RemoteMediator

--- app/src/main/java/anzhy/dizi/ktorapp/MainActivity.kt
+++ app/src/main/java/anzhy/dizi/ktorapp/MainActivity.kt
@@ -3,12 +3,7 @@ package anzhy.dizi.ktorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
--- app/src/main/java/anzhy/dizi/ktorapp/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/anzhy/dizi/ktorapp/data/paging_source/HeroRemoteMediator.kt
@@ -1,5 +1,6 @@
 package anzhy.dizi.ktorapp.data.paging_source
 
+import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
@@ -9,6 +10,8 @@ import anzhy.dizi.ktorapp.data.local.AnimeDatabase
 import anzhy.dizi.ktorapp.data.remote.AnimeApi
 import anzhy.dizi.ktorapp.domain.model.Hero
 import anzhy.dizi.ktorapp.domain.model.HeroRemoteKeys
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @OptIn(ExperimentalPagingApi::class)
@@ -16,6 +19,25 @@ class HeroRemoteMediator @Inject constructor(
     private val animeApi: AnimeApi,
     private val animeDatabase: AnimeDatabase
 ) : RemoteMediator<Int, Hero>() {
+    //used to check whether cached data is out of date and decide whether to trigger a remote refresh
+    //this fun runs before any loading is performed. so we can manipulate data before triggering local or remote loads
+    //this fun is async
+    override suspend fun initialize(): InitializeAction {
+        val currentTime = System.currentTimeMillis()
+        val lastUpdated = remoteKeysDao.getRemoteKey(heroId = 1)?.lastUpdated ?: 0L
+        //only when this cachetimeout expired only then we will load new data from server.
+        //we can set here hours/days/week/month....
+        //val cacheTimeout = 5 - refresh data after 5 min
+        val cacheTimeout = 1440
+
+        val diffInMinutes = (currentTime - lastUpdated) / 1000 / 60
+
+        return if (diffInMinutes.toInt() <= cacheTimeout) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
 
     private val heroDao = animeDatabase.heroDao()
     private val remoteKeysDao = animeDatabase.heroRemoteKeysDao()
@@ -57,7 +79,8 @@ class HeroRemoteMediator @Inject constructor(
                         HeroRemoteKeys(
                             id = hero.id,
                             prevPage = prevPage,
-                            nextPage = nextPage
+                            nextPage = nextPage,
+                            lastUpdated = response.lastUpdated
                         )
                     }
                     remoteKeysDao.addAllRemoteKeys(heroRemoteKeys = keys)
@@ -97,4 +120,12 @@ class HeroRemoteMediator @Inject constructor(
                 remoteKeysDao.getRemoteKey(heroId = hero.id)
             }
     }
+
+    /*
+    private fun parseMills(millis: Long): String {
+        val date = Date(millis)
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.ROOT)
+        return format.format(date)
+    }
+     */
 }
