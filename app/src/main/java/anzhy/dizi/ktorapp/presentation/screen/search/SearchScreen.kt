7b2c9b664ac9b93f118306786ecf82debd3ced95
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-22 16:55:03 -0400

    add search view model

--- app/src/main/java/anzhy/dizi/ktorapp/presentation/screen/search/SearchScreen.kt
+++ app/src/main/java/anzhy/dizi/ktorapp/presentation/screen/search/SearchScreen.kt
@@ -2,19 +2,33 @@ package anzhy.dizi.ktorapp.presentation.screen.search
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 
 @Composable
-fun SearchScreen() {
+fun SearchScreen(
+    navController: NavHostController,
+    searchViewModel: SearchViewModel = hiltViewModel()
+) {
+    val searchQuery by searchViewModel.searchQuery
+
     Scaffold(
         topBar = {
             SearchTopBar(
-                text = "",
-                onTextChange = {},
+                text = searchQuery,
+                onTextChange = {
+                               searchViewModel.updateSearchQuery(query = it)
+                },
                 onSearchClicked = {},
-                onCloseClicked = {}
+                onCloseClicked = {
+                    //popBackStack will just remove our search screen from backstack and we're going to see screen which was
+                    //behind this screen(Home Screen)
+                    navController.popBackStack()
+                }
             )
         }
     ) {
-        
+
     }
 }
---
+++ app/src/main/java/anzhy/dizi/ktorapp/presentation/screen/search/SearchViewModel.kt
@@ -0,0 +1,18 @@
+package anzhy.dizi.ktorapp.presentation.screen.search
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor() : ViewModel() {
+
+    private val _searchQuery = mutableStateOf("")
+    val searchQuery = _searchQuery
+
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
+
+}
