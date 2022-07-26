d8b52fcc0da2fdf29e6f1d91bd832fba9a3632ae
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-26 07:25:42 -0400

    create fake api for testing

---
+++ app/src/test/java/anzhy/dizi/ktorapp/data/remote/FakeAnimeApi.kt
@@ -0,0 +1,79 @@
+package anzhy.dizi.ktorapp.data.remote
+
+import anzhy.dizi.ktorapp.domain.model.ApiResponse
+import anzhy.dizi.ktorapp.domain.model.Hero
+
+class FakeAnimeApi : AnimeApi {
+
+    //fake list of heroes.
+    private val heroes = listOf(
+        Hero(
+            id = 1,
+            name = "Sasuke",
+            image = "",
+            about = "",
+            rating = 5.0,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        ),
+        Hero(
+            id = 2,
+            name = "Naruto",
+            image = "",
+            about = "",
+            rating = 5.0,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        ),
+        Hero(
+            id = 3,
+            name = "Sakura",
+            image = "",
+            about = "",
+            rating = 4.5,
+            power = 0,
+            month = "",
+            day = "",
+            family = listOf(),
+            abilities = listOf(),
+            natureTypes = listOf()
+        )
+    )
+
+    override suspend fun getAllHeroes(page: Int): ApiResponse {
+        return ApiResponse(
+            success = false
+        )
+    }
+
+    override suspend fun searchHeroes(name: String): ApiResponse {
+        val searchedHeroes = findHeroes(name = name)
+        return ApiResponse(
+            success = true,
+            message = "ok",
+            heroes = searchedHeroes
+        )
+    }
+
+    private fun findHeroes(name: String): List<Hero> {
+        val founded = mutableListOf<Hero>()
+        return if (name.isNotEmpty()) {
+            heroes.forEach { hero ->
+                if (hero.name.lowercase().contains(name.lowercase())) {
+                    founded.add(hero)
+                }
+            }
+            founded
+        } else {
+            emptyList()
+        }
+    }
+}
